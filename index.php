7cd53bd65f50ff0c55a971d4ce10c28c1836e0bc
rhidayat122 <151610243@smkassalaambandung.sch.id>
2017-10-24 09:28:53 +0400

    Sayang Sri Mukti Rahayu Selamanya

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+	class robot{
+
+		public $suara;
+		public $berat;
+
+		public function set_suara($suara){
+			$this->suara =$suara;
+		}
+		public function get_Suara(){
+			return  $this->suara;
+		}
+
+	}
+	
+
+?>
